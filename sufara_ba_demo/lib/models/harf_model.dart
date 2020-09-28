ce30a869faac8906a993797d511c7b2c12c0f77e
Amer Beso <abeso1@etf.unsa.ba>
2020-09-28 11:45:26 +0200

    added harf model which will be used for Lekcije and Vjezbe

---
+++ sufara_ba_demo/lib/models/harf_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HarfModel {
+  final String id;
+  final String name;
+  final String imageUrl;
+  final String lekcijaIndex;
+
+  HarfModel({
+    @required this.id,
+    @required this.name,
+    @required this.imageUrl,
+    @required this.lekcijaIndex,
+  });
+}
