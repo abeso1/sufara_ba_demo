eabe1c5609329a5adfb86e50d7e03dd31440d7e9
Amer Beso <abeso1@etf.unsa.ba>
2020-09-28 16:21:43 +0200

    gradient looks better, added one container with title and harf which we are doing vjezba for

--- sufara_ba_demo/lib/screens/vjezba_screen.dart
+++ sufara_ba_demo/lib/screens/vjezba_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sufara_ba_demo/models/harf_model.dart';
+import 'package:sufara_ba_demo/settings/size_config.dart';
 
 class VjezbaScreen extends StatefulWidget {
   final HarfModel harf;
@@ -13,31 +14,81 @@ class VjezbaScreen extends StatefulWidget {
 class _VjezbaScreenState extends State<VjezbaScreen> {
   @override
   Widget build(BuildContext context) {
+    SizeConfig().init(context);
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          '${widget.harf.id}. vjezba',
-          //textAlign: TextAlign.right,
-          style: TextStyle(
-            fontWeight: FontWeight.w300,
+        appBar: AppBar(
+          elevation: 0,
+          centerTitle: true,
+          title: Text(
+            '${widget.harf.id}. vjezba',
+            //textAlign: TextAlign.right,
+            style: TextStyle(
+              fontWeight: FontWeight.w300,
+              color: Colors.white,
+            ),
           ),
-        ),
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              //ovaj gradient treba popraviti
-              colors: [Colors.green[800], Colors.lightGreen],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                //ovaj gradient treba popraviti
+                colors: [Colors.green[800], Colors.lightGreen],
+              ),
             ),
           ),
         ),
-      ),
-      body: Center(
-        child: Text(
-          widget.harf.name,
-          style: TextStyle(fontSize: 36),
-        ),
-      ),
-    );
+        body: Column(
+          children: [
+            Container(
+              height: SizeConfig.blockSizeVertical * 15,
+              width: SizeConfig.blockSizeHorizontal * 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30),
+                ),
+                gradient: LinearGradient(
+                  //ovaj gradient treba popraviti
+                  colors: [Colors.green[800], Colors.lightGreen],
+                ),
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(
+                        left: SizeConfig.blockSizeHorizontal * 6),
+                    width: SizeConfig.blockSizeHorizontal * 100,
+                    child: Text(
+                      'Vjezba',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: SizeConfig.blockSizeVertical * 1,
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(
+                        left: SizeConfig.blockSizeHorizontal * 6),
+                    width: SizeConfig.blockSizeHorizontal * 100,
+                    child: Text(
+                      widget.harf.name,
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            //ovdje idu 3 kartice i button za izgovor
+          ],
+        ));
   }
 }
