a2292c3bdc64914faed501fe85b36ffccb936264
fcidic <faruk.cidic@gmail.com>
2020-09-29 11:19:54 +0200

    singleChildScrollView with ListView. Prodji kroz kod pa vidi kako funkcionise. Ako bude nejasnoca, pitaj slobodno!

--- sufara_ba_demo/lib/screens/lekcija_screen.dart
+++ sufara_ba_demo/lib/screens/lekcija_screen.dart
@@ -16,70 +16,77 @@ class _LekcijaScreenState extends State<LekcijaScreen> {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          centerTitle: true,
-          title: Text(
-            '${widget.harf.id}. lekcija',
-            //textAlign: TextAlign.right,
-            style: TextStyle(
-              fontWeight: FontWeight.w300,
-              color: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          '${widget.harf.id}. lekcija',
+          //textAlign: TextAlign.right,
+          style: TextStyle(
+            fontWeight: FontWeight.w300,
+            color: Colors.white,
+          ),
+        ),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              //ovaj gradient treba popraviti
+              colors: [Colors.green[800], Colors.lightGreen],
             ),
           ),
-          flexibleSpace: Container(
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: SizeConfig.blockSizeVertical * 15,
+            width: SizeConfig.blockSizeHorizontal * 100,
             decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              ),
               gradient: LinearGradient(
                 //ovaj gradient treba popraviti
                 colors: [Colors.green[800], Colors.lightGreen],
               ),
             ),
-          ),
-        ),
-        body: Column(
-          children: [
-            Container(
-              height: SizeConfig.blockSizeVertical * 15,
-              width: SizeConfig.blockSizeHorizontal * 100,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(30),
-                  bottomRight: Radius.circular(30),
-                ),
-                gradient: LinearGradient(
-                  //ovaj gradient treba popraviti
-                  colors: [Colors.green[800], Colors.lightGreen],
-                ),
-              ),
-              child: Column(
-                children: [
-                  //SizedBox(
-                  //  height: SizeConfig.blockSizeVertical * 1,
-                  //),
-                  Container(
-                    padding: EdgeInsets.only(
-                        left: SizeConfig.blockSizeHorizontal * 6),
-                    width: SizeConfig.blockSizeHorizontal * 100,
-                    child: Text(
-                      widget.harf.name,
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.white,
-                      ),
+            child: Column(
+              children: [
+                //SizedBox(
+                //  height: SizeConfig.blockSizeVertical * 1,
+                //),
+
+                Container(
+                  padding:
+                      EdgeInsets.only(left: SizeConfig.blockSizeHorizontal * 6),
+                  width: SizeConfig.blockSizeHorizontal * 100,
+                  // height: SizeConfig.blockSizeVertical * 40,
+                  child: Text(
+                    widget.harf.name,
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Container(
+          ),
+          SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Container(
+              height: SizeConfig.blockSizeVertical * 75,
               padding: EdgeInsets.symmetric(
                 vertical: SizeConfig.blockSizeVertical * 3,
                 horizontal: SizeConfig.blockSizeHorizontal * 3,
               ),
               width: SizeConfig.blockSizeHorizontal * 100,
-              child: Column(
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
                 children: [
                   //ovdje naslov harfa
                   Text(
@@ -99,13 +106,29 @@ class _LekcijaScreenState extends State<LekcijaScreen> {
                       fontWeight: FontWeight.w300,
                     ),
                   ),
+                  Text(
+                    widget.harf.opis,
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
+                  Text(
+                    widget.harf.opis,
+                    style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.w300,
+                    ),
+                  ),
                   //slika grla
                   //oblici
                   //zapocni vjezbu
                 ],
               ),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
