6a3aaf62ef88e2db2b32018f946fd60cf9432644
Amer Beso <abeso1@etf.unsa.ba>
2020-10-04 16:54:31 +0200

    added ribbon for declaring is some Vjezba done or not

--- sufara_ba_demo/lib/widgets/harf_for_lekcije.dart
+++ sufara_ba_demo/lib/widgets/harf_for_lekcije.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:ribbon/ribbon.dart';
 
 import 'package:sufara_ba_demo/models/harf_model.dart';
 import 'package:sufara_ba_demo/screens/lekcija_screen.dart';
 import 'package:sufara_ba_demo/settings/size_config.dart';
+import 'package:sufara_ba_demo/shared/constants.dart';
 
 class HarfWidgetForLekcije extends StatelessWidget {
   final HarfModel harf;
@@ -31,61 +33,73 @@ class HarfWidgetForLekcije extends StatelessWidget {
           ),
           elevation: 15,
           //shadowColor: Colors.green,
-          child: Row(
-            children: [
-              Column(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.only(
-                      left: SizeConfig.blockSizeHorizontal * 7,
-                      top: SizeConfig.blockSizeHorizontal * 1,
-                    ),
-                    child: SvgPicture.asset(
-                      harf.imageUrl,
-                      width: SizeConfig.blockSizeHorizontal * 34,
-                      height: SizeConfig.blockSizeVertical * 12,
-                      color: Colors.green,
-                    ),
-                  )
-                ],
-              ),
-              Column(
-                children: [
-                  Row(
-                    children: [
-                      Container(
+
+          //*//*Ovako se dodaje ribon na lekcije ako treba */
+
+          //child: Ribbon(
+          //  farLength: 60,
+          //  nearLength: 30,
+          //  title: 'Zavrseno',
+          //  titleStyle: TextStyle(
+          //    color: Colors.green,
+          //  ),
+          //  location: RibbonLocation.topEnd,
+           child: Row(
+              children: [
+                Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: SizeConfig.blockSizeHorizontal * 7,
+                        top: SizeConfig.blockSizeHorizontal * 1,
+                      ),
+                      child: SvgPicture.asset(
+                        harf.imageUrl,
                         width: SizeConfig.blockSizeHorizontal * 34,
-                        height: SizeConfig.blockSizeVertical * 3,
-                        margin:
-                            EdgeInsets.all(SizeConfig.blockSizeVertical * 1.5),
-                        child: Text(
-                          '${harf.id}. lekcija',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: Colors.grey,
+                        height: SizeConfig.blockSizeVertical * 12,
+                        color: Colors.green,
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          width: SizeConfig.blockSizeHorizontal * 34,
+                          height: SizeConfig.blockSizeVertical * 3,
+                          margin: EdgeInsets.all(
+                              SizeConfig.blockSizeVertical * 1.5),
+                          child: Text(
+                            '${harf.id}. lekcija',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        harf.name,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 26,
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          harf.name,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 26,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
-      ),
+      //),
     );
   }
 }
--- sufara_ba_demo/lib/widgets/harf_for_vjezbe.dart
+++ sufara_ba_demo/lib/widgets/harf_for_vjezbe.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:ribbon/ribbon.dart';
 import 'package:sufara_ba_demo/models/harf_model.dart';
 import 'package:sufara_ba_demo/screens/vjezba_screen.dart';
 import 'package:sufara_ba_demo/settings/size_config.dart';
+import 'package:sufara_ba_demo/shared/constants.dart';
 
 class HarfWidgetForVjezbe extends StatelessWidget {
   final HarfModel harf;
@@ -26,32 +28,44 @@ class HarfWidgetForVjezbe extends StatelessWidget {
           ),
           elevation: 15,
           //shadowColor: Colors.green,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Text(
-                  '${harf.id}. vjezba',
-                  style: TextStyle(
-                    color: Colors.grey,
+          child: Ribbon(
+            farLength: 60,
+            nearLength: 30,
+            title: 'Zavrseno',
+            color: Colors.green[300],
+            titleStyle: TextStyle(
+              //backgroundColor: Colors.red,
+              color: Colors.white,
+              fontSize: 13,
+            ),
+            location: RibbonLocation.topEnd,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Text(
+                    '${harf.id}. vjezba',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                SvgPicture.asset(
-                  harf.imageUrl,
-                  color: Colors.green,
-                  width: SizeConfig.blockSizeHorizontal * 17,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  harf.name,
-                  style: TextStyle(color: Colors.black, fontSize: 25),
-                ),
-              ],
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SvgPicture.asset(
+                    harf.imageUrl,
+                    color: Colors.green,
+                    width: SizeConfig.blockSizeHorizontal * 17,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    harf.name,
+                    style: TextStyle(color: Colors.black, fontSize: 25),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
