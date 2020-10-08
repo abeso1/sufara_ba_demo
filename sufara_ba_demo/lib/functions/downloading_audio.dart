2e054de4ac51a66b6452658b4c4f620303066d12
Amer Beso <abeso1@etf.unsa.ba>
2020-10-08 14:59:08 +0200

    added method gor unzipping

--- sufara_ba_demo/lib/functions/downloading_audio.dart
+++ sufara_ba_demo/lib/functions/downloading_audio.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:async';
+import 'package:archive/archive.dart';
 import 'package:path_provider/path_provider.dart' as path;
 import 'package:firebase_storage/firebase_storage.dart';
 
@@ -11,25 +12,37 @@ class Download {
     return downloadDir.existsSync();
   }
 
-  Future<void> downloadFile() async{
-    StorageReference storageReference = FirebaseStorage.instance.ref().child('audio');
+  Future<void> downloadFile() async {
+    StorageReference storageReference =
+        FirebaseStorage.instance.ref().child('audio');
     final Directory directory = await path.getExternalStorageDirectory();
     final String downloadPath = '${directory.path}/audio';
 
-
-    try{
+    try {
       final File file = File('$downloadPath/audio');
-      
-      if(file.existsSync()){
-      }
-      else{
+
+      if (file.existsSync()) {
+      } else {
         await file.create(recursive: true);
 
         storageReference.writeToFile(file);
       }
-
     } catch (error) {
       print(error.toString());
+    }
+  }
+
+  unZipFile(var zippedFile) async {
+    Directory directory = await path.getExternalStorageDirectory();
+    var bytes = zippedFile.readAsBytesSync();
+    var archive = ZipDecoder().decodeBytes(bytes);
+    for (var file in archive) {
+      var fileName = '$directory/${file.name}';
+      if (file.isFile) {
+        var outFile = File(fileName);
+        outFile = await outFile.create(recursive: true);
+        await outFile.writeAsBytes(file.content);
+      }
     }
   }
 }
