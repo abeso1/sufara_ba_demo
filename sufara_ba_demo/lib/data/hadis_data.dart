36f36d89ebd354e3a0a431da68a5c6c55174280e
Amer Beso <abeso1@etf.unsa.ba>
2020-10-13 16:53:59 +0200

    random hadis is generated

--- sufara_ba_demo/lib/data/hadis_data.dart
+++ sufara_ba_demo/lib/data/hadis_data.dart
@@ -8,5 +8,104 @@ class Hadis {
       name:
           'Učite Kur\'an, jer će, uistinu, on doći na Sudnji dan kao zagovornik onima koji ga budu učili.',
     ),
+    HadisModel(
+      id: '2',
+      name:
+      'Uistinu, šejtan ne ulazi u kuću u kojoj se uči sura El-Bekare.'
+           ),
+    HadisModel(
+      id: '3',
+      name:
+          "Tako mi Onoga u čijoj je ruci moja duša! Uistinu, sura Ihlas je jednaka trećini Kur'ana (po vrijednosti)."
+          ),
+    HadisModel(
+      id: '4',
+      name:
+      'Učaču Kur’ana će na Sudnjem danu biti rečeno: ‘Uči i penji se džennetskim stepenicama uči lijepo i pravilno, kao što si i na dunjaluku učio, jer, tvoje mjesto u džennetu je kod posljednjeg ajeta kojeg proučiš.'
+    ),
+    HadisModel(
+      id: '5',
+      name:
+       'Ko krene putem na kojem traži znanje, Allah će ga, doista, usmjeriti ka putu od puteva koji vode u Džennet.'   ),
+    HadisModel(
+      id: '6',
+      name:
+          'Doista meleki prostiru svoja krila od dragosti pred onim koji traži znanje. Za učenjaka traže oprosta i oni koji su na nebesima i na Zemlji, pa čak i ribe u morskim dubinama.',
+    ),
+    HadisModel(
+      id: '7',
+      name:
+          'Kome Allah želi dobro poduči ga propisima vjere.',
+    ),
+    HadisModel(
+      id: '8',
+      name:
+          'Najbolji od vas su oni koji nauče Kur’an i njemu druge podučavaju',
+    ),
+    HadisModel(
+      id: '9',
+      name:
+          'Vjernik koji uči Kur\'an je poput dunje, lijepog je i okusa i mirisa.',
+    ),
+    HadisModel(
+      id: '10',
+      name:
+          'Ko prouči deset ajeta u jednoj noći, neće se računati u one koji su nemarni.',
+    ),
+    HadisModel(
+      id: '11',
+      name:
+          'Ko prouči jedan harf iz Kur\'ana za to će imati jedno dobro djelo, a dobro djelo se nagrađuje deseterostruko.',
+    ),
+    HadisModel(
+      id: '12',
+      name:
+          'Učite suru El-Bekare, jer je njeno učenje bereket, a njeno zapostavljanje gubitak. Protiv nje sihirbazi ništa ne mogu.',
+    ),
+    HadisModel(
+      id: '13',
+      name:
+          'Ko nauči napamet prvih deset ajeta sure El-Kehf bit će zaštićen od Dedždžala.',
+    ),
+    HadisModel(
+      id: '14',
+      name:
+          'Ja sam za svoje dobre robove, u džennetu pripremio blagodati kakve oko nije vidjelo, uho nije čulo, niti je ikome na um palo.',
+    ),
+    HadisModel(
+      id: '15',
+      name:
+          'A ko govori ljepše od onoga koji poziva Allahu, Koji dobra djela čini i koji govori: \“Ja sam doista musliman.\” (Fussilet, 33)',
+    ),
+    HadisModel(
+      id: '16',
+      name:
+          'Hvala Allahu koji nas je uputio na pravi put. Da nas Allah nije uputio, mi pravim putem ne bismo išli. (A\’raf, 43)',
+    ),
+    HadisModel(
+      id: '17',
+      name:
+          'Mi tebi objavljujemo Knjigu kao objašnjenje za sve i kao uputu i milost i radosnu vijest. (En-Nahl, 89)',
+    ),
+    HadisModel(
+      id: '18',
+      name:
+          'Sjećajte se vi Mene, i Ja ću se vas sjetiti, i zahvaljujte Mi, i na blagodatima Mojim nezahvalni nemojte biti. (El-Bekara 152)',
+    ),
+    HadisModel(
+      id: '19',
+      name:
+          'On je s vama gdje god bili, i sve što radite Allah vidi. (El-Hadid, 4)',
+    ),
+    HadisModel(
+      id: '20',
+      name:
+          'Zar nagrada za dobro učinjeno djelo može biti nešto drugo do dobro?! (Er-Rahman, 60)',
+    ),
+    HadisModel(
+      id: '21',
+      name:
+          'Postići će šta želi onaj ko se očisti (od loših osobina) i spomene Ime Gospodara svoga pa Namaz obavi. (El-A\’la 14-15)',
+    ),
   ];
 }
--- sufara_ba_demo/lib/screens/splash_screen.dart
+++ sufara_ba_demo/lib/screens/splash_screen.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
-
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:sufara_ba_demo/data/hadis_data.dart';
 import 'package:sufara_ba_demo/screens/tabs_screen.dart';
 import 'package:sufara_ba_demo/settings/size_config.dart';
 
 class SplashScreen extends StatelessWidget {
+  var rng = new Random();
+
   @override
   Widget build(BuildContext context) {
     Timer(Duration(seconds: 5), (){
@@ -24,7 +26,7 @@ class SplashScreen extends StatelessWidget {
               'svg/back_img/sufara.ba_logo.png',
             ),
             Text(
-              Hadis.listHadis[0].name,
+              Hadis.listHadis[rng.nextInt(20)].name,
             ),
           ],
         ),
