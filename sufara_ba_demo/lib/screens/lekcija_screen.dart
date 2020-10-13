c8a6493f32303063edbdb0fe64b62bb70d01542b
Amer Beso <abeso1@etf.unsa.ba>
2020-10-13 17:01:49 +0200

    roboto from google added to whole project

--- sufara_ba_demo/lib/screens/lekcija_screen.dart
+++ sufara_ba_demo/lib/screens/lekcija_screen.dart
@@ -40,7 +40,7 @@ class _LekcijaScreenState extends State<LekcijaScreen> {
           //textAlign: TextAlign.right,
           style: TextStyle(
             fontWeight: FontWeight.w300,
-            fontFamily: 'RobotoCondensed',
+            fontFamily: 'Roboto',
             color: Colors.white,
           ),
         ),
@@ -86,7 +86,7 @@ class _LekcijaScreenState extends State<LekcijaScreen> {
                       fontSize: 30,
                       fontWeight: FontWeight.bold,
                       color: Colors.white,
-                      fontFamily: 'RobotoCondensed',
+                      fontFamily: 'Roboto',
                     ),
                   ),
                 ),
@@ -175,7 +175,7 @@ class _LekcijaScreenState extends State<LekcijaScreen> {
                     widget.harf.lekcijaIliVjezbaIndex,
                     style: TextStyle(
                       fontSize: 24,
-                      fontFamily: 'RobotoCondensed',
+                      fontFamily: 'Roboto',
                       fontWeight: FontWeight.w700
                     ),
                   ),
@@ -190,7 +190,7 @@ class _LekcijaScreenState extends State<LekcijaScreen> {
                     style: TextStyle(
                       fontSize: 22,
                       fontWeight: FontWeight.normal,
-                      fontFamily: 'RobotoCondensed',
+                      fontFamily: 'Roboto',
                     ),
                   ),
                   //slika grla
@@ -303,7 +303,7 @@ class _LekcijaScreenState extends State<LekcijaScreen> {
                             color: Colors.white,
                             fontSize: 20,
                             fontWeight: FontWeight.normal,
-                            fontFamily: 'RobotoCondensed',
+                            fontFamily: 'Roboto',
                           ),
                         ),
                       ),
