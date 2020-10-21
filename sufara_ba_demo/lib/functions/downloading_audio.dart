7b75c5f2df690f1415bfd2d145f7440040a67441
Amer Beso <abeso1@etf.unsa.ba>
2020-10-21 18:47:12 +0200

    provider does not work

--- sufara_ba_demo/lib/functions/downloading_audio.dart
+++ sufara_ba_demo/lib/functions/downloading_audio.dart
@@ -1,5 +1,6 @@
 import 'dart:io' as io;
 import 'dart:async';
+import 'package:archive/archive.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
@@ -8,7 +9,7 @@ import 'package:path_provider/path_provider.dart' as path;
 
 class Download extends ChangeNotifier {
   static var httpClient = new io.HttpClient();
-  double progress = 0.0;
+  double progress = 0.11;
   var length;
   var dio = Dio();
 
@@ -26,67 +27,35 @@ class Download extends ChangeNotifier {
     //    io.Directory('$dir/svg/1').existsSync());
     return false;
   }
-}
-/*
-//ovo radi
+
+  //ovo radi
   Future<io.File> downloadFile(String url, String filename) async {
-    /*String dir = (await path.getApplicationDocumentsDirectory()).path;
-    io.File file = new io.File('$dir/$filename');
-    try {
-      Response response = await dio.get(
-        url,
-
-        onReceiveProgress: showDownloadProgress,
-
-        options: Options(
-          responseType: ResponseType.bytes,
-          followRedirects: false,
-          validateStatus: (status) {
-            return status < 500;
-          },
-        ),
-      );
-      var raf = file.openSync(mode: io.FileMode.write);
-      raf.writeFromSync(response.data);
-      await raf.close();
-    } catch (e) {
-      print(e);
-    }*/
-    ////ovo radi za firebase
-    ///ovaj za sad radi, ali ne znam kako progres da ispisem
     var request = await httpClient.getUrl(Uri.parse(url));
-    progress = 0.1;
-//notifyListeners();
+
+    progress += 0.03;
+    notifyListeners();
     var response = await request.close();
-    print('2$progress');
-    progress = 0.2;
-//notifyListeners();
+    print('$progress');
+    progress += 0.03;
+    notifyListeners();
     var bytes = await consolidateHttpClientResponseBytes(response);
     print('3$progress');
-    progress = 0.3;
-//notifyListeners();
+    progress += 0.03;
+    notifyListeners();
     String dir = (await path.getApplicationDocumentsDirectory()).path;
     print('4$progress');
-    progress = 0.4;
-    //notifyListeners();
+    progress += 0.03;
+    notifyListeners();
     io.File file = new io.File('$dir/$filename');
     print('5$progress');
-    progress = 0.5;
-    //notifyListeners();
+    progress += 0.03;
+    notifyListeners();
     await file.writeAsBytes(bytes);
     print('6$progress');
-    progress = 0.6;
-    //notifyListeners();
+    progress += 0.03;
+    notifyListeners();
     print('$dir/$filename');
     return file;
-  }
-
-  void showDownloadProgress(received, total) {
-    if (total != -1) {
-      print((received / total * 100).toStringAsFixed(0) + "%");
-    }
-    progress = (received / total * 100);
-    notifyListeners();
   }
 
 //ovo radi
@@ -94,12 +63,13 @@ class Download extends ChangeNotifier {
     String directory = (await path.getApplicationDocumentsDirectory()).path;
     var bytes = zippedFile.readAsBytesSync();
     var archive = ZipDecoder().decodeBytes(bytes);
-    var archiveLength = archive.length * 2;
+    //setState(() {
+    //  text = 'Skoro gotovo...';
+    //});
     for (var file in archive) {
       var fileName = '$directory/${file.name}';
-      //var fileLength = file.size;
-      //progress += (fileLength / archiveLength);
-      notifyListeners();
+        progress += 0.0003;
+        notifyListeners();
       if (file.isFile) {
         var outFile = io.File(fileName);
         outFile = await outFile.create(recursive: true);
@@ -107,19 +77,14 @@ class Download extends ChangeNotifier {
       }
       print('$directory/${file.name}');
     }
+    //if (progress > 0.97) Navigator.of(context).pop();
     return true;
   }
 
-  Future<bool> downloadAndUnzip() async {
-    /*io.File file = await downloadFile(
-      //'https://firebasestorage.googleapis.com/v0/b/sufaramobile.appspot.com/o/audio.zip?alt=media&token=016531db-bde8-4bb3-82f2-7bc8ddf770a9',
-      'https://drive.google.com/u/1/uc?id=14IvtFtMmhs49opKX0Sts8UihRlDICqik&export=download',
-      'audio.zip',
-    );
-
-    await unZipFile(file);*/
-//////////https://drive.google.com/u/1/uc?id=14IvtFtMmhs49opKX0Sts8UihRlDICqik&export=download
+  Future<bool> downloadAndUnzipAudio() async {
     downloadFile(
+      //ovaj firebaseov link radi, ali za samo mali broj korisnika
+      //'https://firebasestorage.googleapis.com/v0/b/sufaramobile.appspot.com/o/audio.zip?alt=media&token=016531db-bde8-4bb3-82f2-7bc8ddf770a9',
       'https://drive.google.com/u/1/uc?id=14IvtFtMmhs49opKX0Sts8UihRlDICqik&export=download',
       'audio.zip',
     ).then(
@@ -130,4 +95,17 @@ class Download extends ChangeNotifier {
 
     return true;
   }
-}*/
+
+  Future<bool> downloadAndUnzipSvg() async {
+    downloadFile(
+      'https://drive.google.com/u/1/uc?id=120Z6q-ZAwt4fazv2WV86P-OZNLX_r8bB&export=download',
+      'svg.zip',
+    ).then(
+      (value) => {
+        unZipFile(value),
+      },
+    );
+
+    return true;
+  }
+}
--- sufara_ba_demo/lib/widgets/progression_indicator.dart
+++ sufara_ba_demo/lib/widgets/progression_indicator.dart
@@ -1,13 +1,15 @@
-import 'dart:async';
+//import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sufara_ba_demo/functions/downloading_audio.dart';
 import 'package:sufara_ba_demo/settings/size_config.dart';
 
-import 'dart:io' as io;
-import 'package:archive/archive.dart';
+//import 'dart:io' as io;
+//import 'package:archive/archive.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
-import 'package:path_provider/path_provider.dart' as path;
+//import 'package:flutter/foundation.dart';
+//import 'package:path_provider/path_provider.dart' as path;
 
 class ProgressionIndicator extends StatefulWidget {
   @override
@@ -15,117 +17,19 @@ class ProgressionIndicator extends StatefulWidget {
 }
 
 class _ProgressionIndicatorState extends State<ProgressionIndicator> {
-  double progress = 0.11;
+  //double progress = 0.11;
   String text = 'Preuzimanje podataka...';
-
-  static var httpClient = new io.HttpClient();
-  Future<bool> checkFile() async {
-    String dir = (await path.getApplicationDocumentsDirectory()).path;
-    /*if (io.Directory('$dir/audio/1').existsSync()) {
-      print('postoji');
-    }*/
-    //ovo treba
-    //return (io.Directory('$dir/audio/1').existsSync() &&
-     //   io.Directory('$dir/svg/1').existsSync());
-    //ovo je za testing samo
-    return false;
-  }
-
-//ovo radi
-  Future<io.File> downloadFile(String url, String filename) async {
-    var request = await httpClient.getUrl(Uri.parse(url));
-    setState(() {
-      progress += 0.03;
-    });
-    var response = await request.close();
-    print('2$progress');
-    setState(() {
-      progress += 0.03;
-    });
-    var bytes = await consolidateHttpClientResponseBytes(response);
-    print('3$progress');
-    setState(() {
-      progress += 0.03;
-    });
-    String dir = (await path.getApplicationDocumentsDirectory()).path;
-    print('4$progress');
-    setState(() {
-      progress += 0.03;
-    });
-    io.File file = new io.File('$dir/$filename');
-    print('5$progress');
-    setState(() {
-      progress += 0.03;
-    });
-    await file.writeAsBytes(bytes);
-    print('6$progress');
-    setState(() {
-      progress += 0.03;
-    });
-    print('$dir/$filename');
-    return file;
-  }
-
-//ovo radi
-  Future<bool> unZipFile(var zippedFile) async {
-    String directory = (await path.getApplicationDocumentsDirectory()).path;
-    var bytes = zippedFile.readAsBytesSync();
-    var archive = ZipDecoder().decodeBytes(bytes);
-    //setState(() {
-    //  text = 'Skoro gotovo...';
-    //});
-    for (var file in archive) {
-      var fileName = '$directory/${file.name}';
-      setState(() {
-        progress += 0.0003;
-      });
-      if (file.isFile) {
-        var outFile = io.File(fileName);
-        outFile = await outFile.create(recursive: true);
-        await outFile.writeAsBytes(file.content);
-      }
-      print('$directory/${file.name}');
-    }
-    if(progress > 0.97) Navigator.of(context).pop();
-    return true;
-  }
-
-  Future<bool> downloadAndUnzipAudio() async {
-    downloadFile(
-      //ovaj firebaseov link radi, ali za samo mali broj korisnika
-      //'https://firebasestorage.googleapis.com/v0/b/sufaramobile.appspot.com/o/audio.zip?alt=media&token=016531db-bde8-4bb3-82f2-7bc8ddf770a9',
-      'https://drive.google.com/u/1/uc?id=14IvtFtMmhs49opKX0Sts8UihRlDICqik&export=download',
-      'audio.zip',
-    ).then(
-      (value) => {
-        unZipFile(value),
-      },
-    );
-
-    return true;
-  }
-
-  Future<bool> downloadAndUnzipSvg() async {
-    downloadFile(
-      'https://drive.google.com/u/1/uc?id=120Z6q-ZAwt4fazv2WV86P-OZNLX_r8bB&export=download',
-      'svg.zip',
-    ).then(
-      (value) => {
-        unZipFile(value),
-      },
-    );
-
-    return true;
-  }
+  Download download = Download();
 
   @override
   void initState() {
-    downloadAndUnzipAudio().then((value) => downloadAndUnzipSvg());
+    download.downloadAndUnzipAudio().then((value) => download.downloadAndUnzipSvg());
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    var progress = Provider.of<Download>(context, listen: false).progress;
     SizeConfig().init(context);
     return SimpleDialog(
       children: [
