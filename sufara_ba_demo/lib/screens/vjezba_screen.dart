f255e47d146cd42c0e5c76417bccc54da9d1379b
Amer Beso <abeso1@etf.unsa.ba>
2020-11-18 16:29:58 +0100

    harf in vjezbascreen dont repeat

--- sufara_ba_demo/lib/screens/vjezba_screen.dart
+++ sufara_ba_demo/lib/screens/vjezba_screen.dart
@@ -31,6 +31,8 @@ class VjezbaScreen extends StatefulWidget {
 
 class _VjezbaScreenState extends State<VjezbaScreen> {
   var rng = new Random();
+  int prethodni;
+  int pretprethodni;
   int index1;
   int index2;
   int index3;
@@ -87,12 +89,20 @@ class _VjezbaScreenState extends State<VjezbaScreen> {
     setState(() {
       index3 = treci;
     });
-    List<int> array = [prvi, drugi, treci];
-    int broj = rng.nextInt(array.length);
-    setState(() {
-      tacan = array[broj];
-      tacanBroj = broj;
-    });
+    bool tacno = true;
+    while (tacno) {
+      List<int> array = [prvi, drugi, treci];
+      int broj = rng.nextInt(array.length);
+      if (array[broj] != prethodni && array[broj] != pretprethodni) {
+        tacno = false;
+        setState(() {
+          tacan = array[broj];
+          tacanBroj = broj;
+          pretprethodni = prethodni;
+          prethodni = array[broj];
+        });
+      }
+    }
     setState(() {
       jedan = CardForVjezbeRegural(widget.harf, index1, widget.dir);
       dva = CardForVjezbeRegural(widget.harf, index2, widget.dir);
@@ -347,7 +357,7 @@ class _VjezbaScreenState extends State<VjezbaScreen> {
                       ),
                       Flexible(
                         child: Text(
-                          'Poslušajte izgovor i odaberite tačan odgovor.',
+                          'Poslušajte izgovor i odaberite tacan odgovor.',
                           style: TextStyle(
                             color: Colors.blue,
                             fontSize: SizeConfig.blockSizeHorizontal * 5,
@@ -418,88 +428,121 @@ class _VjezbaScreenState extends State<VjezbaScreen> {
                     height: SizeConfig.blockSizeVertical * 1,
                   ),
                   Container(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: SizeConfig.blockSizeHorizontal * 2,
-                      vertical: SizeConfig.blockSizeVertical * 1,
-                    ),
-                    width: SizeConfig.safeBlockHorizontal * 95,
-                    height: SizeConfig.blockSizeVertical * 4,
-                    child: LiquidLinearProgressIndicator(
-                      //minHeight: SizeConfig.blockSizeVertical * 2,
-                      backgroundColor: _backValueColors[indexColor],
-                      value: indexColor / 5,
-                      valueColor: _valueColors[indexColor],
-                      direction: Axis.horizontal,
-                      borderRadius: 10,
-                    ),
-                  ),
-                  SizedBox(
-                    height: SizeConfig.blockSizeVertical * 1,
-                  ),
-                  Container(
-                    height: SizeConfig.blockSizeVertical * 6,
-                    padding: EdgeInsets.symmetric(
-                      horizontal: SizeConfig.blockSizeVertical * 7,
-                    ),
-                    child: RaisedButton(
-                      onPressed: () {
-                        playAudio(widget.harf, tacan);
-                      },
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      padding: EdgeInsets.all(0),
-                      elevation: 5,
-                      child: Container(
-                        //padding: EdgeInsets.symmetric(
-                        //  horizontal: SizeConfig.blockSizeVertical * 7,
-                        //  vertical: SizeConfig.blockSizeVertical * 1.25,
-                        //),
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(15),
-                          gradient: LinearGradient(
-                            colors: [kon_boja, poc_boja],
-                          ),
-                          //boxShadow: [
-                          //BoxShadow(
-                          //color: Colors.lightGreen[100],
-                          //blurRadius: 2.0,
-                          //spreadRadius: 0.0,
-                          //offset: Offset(
-                          //    2.0, 2.0), // shadow direction: bottom right
-                          //)
-                          //],
+                    //width: SizeConfig.safeBlockHorizontal * 50,
+                    //height: SizeConfig.blockSizeVertical * 4,
+                    child: Row(
+                      children: [
+                        SizedBox(
+                          width: SizeConfig.blockSizeHorizontal * 5,
                         ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Icon(
-                              Icons.replay,
-                              color: Colors.white,
-                              size: SizeConfig.blockSizeVertical * 4.5,
-                            ),
-                            Center(
-                              child: FittedBox(
-                                fit: BoxFit.fill,
-                                child: Text(
-                                  'IZGOVOR',
-                                  textAlign: TextAlign.center,
-                                  style: TextStyle(
-                                    fontFamily: 'Roboto',
-                                    fontWeight: FontWeight.normal,
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                  ),
+                        Container(
+                          //padding: EdgeInsets.symmetric(
+                          //  horizontal: SizeConfig.blockSizeHorizontal * 2,
+                          //  vertical: SizeConfig.blockSizeVertical * 1,
+                          //),
+                          width: 50,
+                          height: 50,
+                          child: LiquidCircularProgressIndicator(
+                            //minHeight: SizeConfig.blockSizeVertical * 2,
+                            backgroundColor: _backValueColors[indexColor],
+                            value: indexColor / 5,
+                            valueColor: _valueColors[indexColor],
+                            center: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                SizedBox(
+                                  width: SizeConfig.blockSizeHorizontal * 3,
                                 ),
+                                Text(
+                                  indexColor.toString(),
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                                Text(
+                                  "/5",
+                                  style: TextStyle(fontSize: 12),
+                                ),
+                              ],
+                            ),
+                            //strokeWidth: 10,
+                            // borderColor: Colors.red,
+                            // borderWidth: 5.0,
+                            // direction: Axis.vertical,
+                            //borderRadius: 10,
+                          ),
+                        ),
+                        SizedBox(
+                          width: SizeConfig.blockSizeHorizontal * 4,
+                        ),
+                        Container(
+                          height: SizeConfig.blockSizeVertical * 6,
+                          width: SizeConfig.blockSizeHorizontal * 45,
+                          //padding: EdgeInsets.symmetric(
+                          //horizontal: SizeConfig.blockSizeVertical * 7,
+                          //),
+                          child: RaisedButton(
+                            onPressed: () {
+                              playAudio(widget.harf, tacan);
+                            },
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                            padding: EdgeInsets.all(0),
+                            elevation: 5,
+                            child: Container(
+                              //padding: EdgeInsets.symmetric(
+                              //  horizontal: SizeConfig.blockSizeVertical * 7,
+                              //  vertical: SizeConfig.blockSizeVertical * 1.25,
+                              //),
+                              width: double.infinity,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                gradient: LinearGradient(
+                                  colors: [kon_boja, poc_boja],
+                                ),
+                                //boxShadow: [
+                                //BoxShadow(
+                                //color: Colors.lightGreen[100],
+                                //blurRadius: 2.0,
+                                //spreadRadius: 0.0,
+                                //offset: Offset(
+                                //    2.0, 2.0), // shadow direction: bottom right
+                                //)
+                                //],
+                              ),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Icon(
+                                    Icons.replay,
+                                    color: Colors.white,
+                                    size: SizeConfig.blockSizeVertical * 4.5,
+                                  ),
+                                  Center(
+                                    child: FittedBox(
+                                      fit: BoxFit.fill,
+                                      child: Text(
+                                        'IZGOVOR',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontFamily: 'Roboto',
+                                          fontWeight: FontWeight.normal,
+                                          color: Colors.white,
+                                          fontSize: 20,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: SizeConfig.blockSizeHorizontal * 1,
+                                  )
+                                ],
                               ),
                             ),
-                            SizedBox(
-                              width: SizeConfig.blockSizeHorizontal * 1,
-                            )
-                          ],
+                          ),
                         ),
-                      ),
+                      ],
                     ),
                   ),
                 ],
