8ff52e6f3d2436987cb63bcfe1e7608d73997a97
Amer Beso <abeso1@etf.unsa.ba>
2020-12-02 21:57:09 +0100

    added icons

--- sufara_ba_demo/lib/functions/downloading_audio.dart
+++ sufara_ba_demo/lib/functions/downloading_audio.dart
@@ -20,7 +20,7 @@ class Download extends ChangeNotifier {
   Future<bool> checkFile() async {
     SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
     bool povratni = sharedPreferences.getBool('preuzeto') ?? false;
-    return false;
+    return povratni;
   }
 
   //ovo radi
